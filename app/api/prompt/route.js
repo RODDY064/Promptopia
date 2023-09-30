e0b6cc4d4e14d9c4d363a11a4a19d30fcdaadf16
Roddy <adarkwagodred7075@icloud.com>
2023-09-30 16:09:18 +0000

    bug fixed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+// new prisma client
 export const GET = async (req) =>{
     try {
       const posts = await prisma.post.findMany({
