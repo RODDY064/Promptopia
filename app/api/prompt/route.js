fe9dee818931f25b69904830fb6d7151fe226f8d
Roddy <adarkwagodred7075@icloud.com>
2023-09-30 20:40:27 +0000

    bug fixed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,8 +8,10 @@ export const GET = async (req) =>{
           author: true // Include the author relation
         }
       });
-     
-      return NextResponse.json(posts)
+      if(posts){
+
+      }
+      return new  NextResponse.json(posts)
     } catch (error) {
       console.error(error);
       return NextResponse.json({ error: "Internal server error" }, { status: 500 });
