2f908f016a615e24c3e62e3f2c608a6ea97da90d
Roddy <adarkwagodred7075@icloud.com>
2023-10-01 12:58:59 +0000

    caches control added

--- app/api/prompt/post/route.js
+++ app/api/prompt/post/route.js
@@ -1,23 +1,22 @@
 import prisma from "@libs/prisma";
 import { NextResponse } from "next/server";
 
-// new prisma client
-export const GET=async (req) =>{
-    try {
+export const GET = async (req, res) => {
+  try {
       const posts = await prisma.post.findMany({
-        include: {
-          author: true // Include the author relation
-        }
+          include: {
+              author: true // Include the author relation
+          }
       });
       if (!posts) {
-        return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+          return NextResponse.json({ error: 'Post not found' }, { status: 404 });
       }
-      response.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-      response.setHeader('Last-Modified', new Date().toUTCString());
- 
-      return NextResponse.json(posts,{ status: 200 });
-    } catch (error) {
+      res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+      res.setHeader('Last-Modified', new Date().toUTCString());
+
+      return NextResponse.json(posts, { status: 200 });
+  } catch (error) {
       console.error(error);
-      return NextResponse.json({ error:"Internal server error" },{ status: 500 });
-    }
+      return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
